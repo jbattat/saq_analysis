f747897ac0f366a25fb5f4cd816e8dd3e4c81b99
James Battat <jbattat@wellesley.edu>
2023-09-28 10:31:41 -0400

    updated with nora's calibration code

--- analyze_root.py
+++ analyze_root.py
@@ -81,22 +81,18 @@ for ch, resets in enumerate(chResets):
     print("Resets per channel", str(ch + 1), ": ", len(resets))
 save_data[:,1] = channel_dist
 
-#channel_dist = channel_dist/max(channel_dist)
-dark = [1.,0.23290509, 0.15205463, 0.17366971, 0.05632563, 0.04472116,
-        0.03598277, 0.02018649, 0.03296155, 0.01581746, 0.00741562, 0.00979818,
-        0.01196661, 0.01263316, 0.,         0.        ]
-#channel_dist = channel_dist - dark 
+
 #print("# of resets per channel - averaged", repr(channel_dist))
 
 #print(channel_dist)
-plt.plot(midpoint, channel_dist, 'o')
+#plt.plot(midpoint, channel_dist, 'o')
 plt.xlim(0, 20)
 plt.xlabel("Distance from center (mm)")
 plt.ylabel("Number of resets per channel averaged over area")
 plt.title("Distribution of resets")
 outfile = input_file.replace('.root', '_dist.pdf')
 plt.savefig(outfile)
-plt.show()
+#plt.show()
 plt.clf()
 
 
@@ -111,9 +107,14 @@ channel1_resets = chResets[0]
 rtd = [ [] for _ in range(16)]
 current  = [ [] for _ in range(16)]
 
+mask = [ [] for _ in range(16)]    
+mean = np.zeros(saq.N_SAQ_CHANNELS)
+std  = np.zeros(saq.N_SAQ_CHANNELS)
+
 for ch in range(16):
     rtd[ch] = np.zeros(len(chResets[ch]))
     current[ch] = np.zeros(len(chResets[ch]))
+    mask[ch] = np.zeros(len(chResets[ch]))
 
 elements = len(max(chResets, key=len))
 #tdr = np.zeros([16, elements])
@@ -125,14 +126,16 @@ cap = 1e-11
 #print(chResets[ch])
 hold = 0
 reset_fixed = np.zeros(16)
-for ch in range(13):
+fig, axs = plt.subplots(2,5, figsize=(12,8))
+    
+for ch in range(10):
     remove_r = []
     for r in range(len(chResets[ch])-1):
         prev = chResets[ch][r+1]
         curr = chResets[ch][r]
         temp  = prev - curr
         rtd[ch][r] = temp
-        if rtd[ch][r] < 1e-2:
+        if rtd[ch][r] < 1e-4:
             remove_r.append(r)
         else:
             if rtd[ch][r] != 0:
@@ -142,14 +145,37 @@ for ch in range(13):
     current[ch] = np.delete(current[ch], remove_r)
     chResets[ch] = np.delete(chResets[ch], remove_r)
     reset_fixed[ch] = len(chResets[ch])/area[ch]
-    plt.step(chResets[ch][1:-1], current[ch][1:-1], where ='post', label = str(ch+1))
+    #plt.step(chResets[ch][1:-1], current[ch][1:-1], where ='post', label = str(ch+1))
+    #plt.plot(chResets[ch][1:-1], current[ch][1:-1], 'o')
+    row = int(ch/5)
+    col = ch%5
+    lower_bound = 1e-4
+    upper_bound = 1e-2
+    mask = (rtd[ch] >= lower_bound) & (rtd[ch] <= upper_bound)
+    mean[ch] = np.mean(rtd[ch][mask])
+
+    std[ch] = np.std(rtd[ch][mask])
+
+
+    #mean[ch] = np.mean(rtd[ch])
+
+    #std[ch] = np.std(rtd[ch])
+    print("ch", ch+1, " ,mean: ",  mean[ch], " ,std: ", std[ch])
+    axs[row,col].hist(rtd[ch][mask], bins = 100)
+    #axs[row,col].hist(rtd[ch], bins = 100)
+    axs[row,col].axvline(mean[ch], color = 'red')
+    axs[row,col].axvline(mean[ch]-std[ch] , color = 'green')
+    axs[row,col].axvline(mean[ch]+std[ch] , color = 'green')
+    
+
 #    print(rtd[ch])
 
 #print(max(rtd[0]))
 #plt.hist(rtd[0], 100)
 #plt.xlim(0, 0.5)
 #print("average rtd: ", np.median(rtd[0]))
-#plt.show()
+plt.show()
+sys.exit()
 #plt.step(chResets[1][:-1], current[1][:-1], where ='post', label = "Channel 1")
 plt.title("Reconstructed Current")
 plt.xlabel("Time (s)")
